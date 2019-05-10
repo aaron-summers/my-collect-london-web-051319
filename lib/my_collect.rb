27f1cf42accd0f010d20dfcd99705b8b9b6ad619
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 02:10:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(collection)
     i = 0
     
     while i < collection.length
-      yield new = collection[i]
+      yield collection[i]
       i += 1
     end
-    return new 
+    return collection
 end
