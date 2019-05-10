4a5a417eba8067340da8b59ae235fa7715c3a541
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 02:09:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,10 @@
-def my_collect
-  
+def my_collect(collection)
+  if block_given?
+    i = 0
+    
+    while i < collection.length
+      yield new = collection[i]
+      i += 1
+    end
+    return new 
 end
