45e97b03e420fdd0c5c26681bce98e492da11810
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-10 02:12:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(collection)
   #if block_given?
     i = 0
+    new_collection = []
     
     while i < collection.length
-      yield collection[i]
+      new_collection << yield(collection[i])
       i += 1
     end
     return collection
